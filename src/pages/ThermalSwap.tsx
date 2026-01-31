ef889e421225a41781a9117c5227fa71b569e37c
KJESS1 <jessicauduud@gmail.com>
2026-01-31 09:55:00 +0100

    Create ThermalSwap token swapping page with snowman mascot

---
+++ src/pages/ThermalSwap.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Snowman } from '../components/Snowman';
+
+const ThermalSwap: React.FC = () => {
+    return (
+        <div>
+            <h1>Token Swapping Page</h1>
+            <Snowman />
+            <p>Welcome to the token swapping page!</p>
+        </div>
+    );
+};
+
+export default ThermalSwap;
